8b5f5dfdd5bdf46a951a6942313275b0e5a2d988
Robert Spring <ironguard20@gmail.com>
2022-01-03 22:16:18 +0000

    mixed lists

---
+++ challenges/chal01.py
@@ -0,0 +1,11 @@
+#/usr/bin/env python3
+"""
+Objective: Write a script that contains the following:
+An input asking the user's name.
+An input asking what day of the week it is.
+A print statement that reads:
+Hello, <name>! Happy <day of the week>!
+"""
+user_name = input("What is your name? ")
+day_of_week = input("What day of the week is it? ")
+print(f"Hello, {user_name}! Happy {day_of_week}!")
---
+++ mix-list/mixlist01.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# create list 
+my_list = [  5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+# using f strings
+print(f"IP addresses: {my_list[3]}, and {my_list[4]}")
+
